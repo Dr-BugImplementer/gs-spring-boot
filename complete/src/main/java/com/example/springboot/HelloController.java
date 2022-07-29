7c347ac18241d939d38eba4ade2764e739a1bc7e
Dr-BugImplementer <malteroeper@hotmail.de>
2022-07-29 11:01:12 +0200

    -

--- complete/src/main/java/com/example/springboot/HelloController.java
+++ complete/src/main/java/com/example/springboot/HelloController.java
@@ -2,12 +2,10 @@ package com.example.springboot;
 
 import com.example.springboot.repository.LoginRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "https://zealous-coast-054484603.1.azurestaticapps.net")
 @RestController
 public class HelloController {
 
