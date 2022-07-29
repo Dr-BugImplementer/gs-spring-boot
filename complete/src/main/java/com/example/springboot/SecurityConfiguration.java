77fd605ee6da6b51c8697848c4f4ffcb161a7325
Dr-BugImplementer <malteroeper@hotmail.de>
2022-07-29 11:36:16 +0200

    corse check

---
+++ complete/src/main/java/com/example/springboot/SecurityConfiguration.java
@@ -0,0 +1,28 @@
+package com.example.springboot;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+import java.util.Arrays;
+
+@Configuration
+public class SecurityConfiguration   {
+    
+    @Value("{app.cors.allowed-origins}")
+    private List<String> corsAllowedOrigins;
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(corsAllowedOrigins);
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
